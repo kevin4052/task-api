95a655a41369ee2f2c6b2e71d94f176761d2ec8c
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-10-12 19:06:35 -0400

    initial commit

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  tasks: {
+    type: [{ type: Schema.Types.ObjectId, ref: "Task"}]
+  }
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
