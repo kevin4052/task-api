084c53c4d8615104e476b02b6f47d60fee532680
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-10-12 20:46:51 -0400

    completed and tested CRUD routes for tasks

--- routes/task-routes/tasks.js
+++ routes/task-routes/tasks.js
@@ -1,19 +1,8 @@
 const express = require('express');
-const { create } = require('../../models/User');
 const router  = express.Router();
 const Task = require('../../models/task.model.js');
 
-/* GET task page */
-router.get('/all-tasks', (req, res, next) => {
-  Task
-    .find()
-    .then(tasksFromDB => {
-      console.log({tasksFromDB});
-      res.status(200).json(tasksFromDB);
-    })
-    .catch(err => console.log(err))
-});
-
+// CREATE task
 router.post('/create', (req, res) => {
   console.log({body: req.body});
 
@@ -22,24 +11,52 @@ router.post('/create', (req, res) => {
       console.log({createdTask});
       res.status(200).json(createdTask);
     })
-    .catch(err => res.status(400).json({message: 'Error while creating task'}))
+    .catch(err => {
+      res.status(400).json({message: 'Error while creating task'});
+    });
 });
 
+// READ tasks
+router.get('/all-tasks', (req, res, next) => {
+
+  Task.find()
+    .then(tasksFromDB => {
+      console.log({tasksFromDB});
+      res.status(200).json(tasksFromDB);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error finding all tasks"});
+    });
+
+});
+
+// UPDATE task
 router.put('/update', (req, res) => {
+
+  console.log({body: req.body})
+
   Task.findByIdAndUpdate(req.body.taskId, req.body, { new: true })
     .then(updatedTask => {
       console.log({updatedTask})
       res.status(200).json(updatedTask);
     })
-    .catch(err => res.status(400).json({message: "Error while updating task"}))
+    .catch(err => {
+      res.status(400).json({message: "Error while updating task"});
+    });
+
 });
 
+// DELETE task
 router.delete('/delete', (req, res) => {
+
   Task.findByIdAndDelete(req.body.taskId)
     .then(() => {
-      res.status(200).json({message: "Delete successfully"})
+      res.status(200).json({message: "Delete successfully"});
     })
-    .catch(err => res.status(400).json({message: "Error while trying to delete task"}))
+    .catch(err => {
+      res.status(400).json({message: "Error while trying to delete task"});
+    });
+
 })
 
 
